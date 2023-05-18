a5e0dae04e85e3295b66bbb3e0f35f701572837f
vaibhav <vaibhav.vyas.180@gmail.com>
2023-05-18 18:49:01 +0530

    added

--- src/com/assignment/Crearture.java
+++ src/com/assignment/Crearture.java
@@ -2,9 +2,9 @@ package com.assignment;
 
 public class Crearture {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+			int a;
 	}
 
 }
